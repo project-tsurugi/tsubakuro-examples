cb06d4a695fe522d10ffc641e991f589a660e6ab
t-horikawa <horikawa@nautilus-technologies.com>
2025-01-30 16:30:58 +0900

    revise examples/tables example

--- modules/clients/src/main/java/com/tsurugidb/tsubakuro/examples/tables/Main.java
+++ modules/clients/src/main/java/com/tsurugidb/tsubakuro/examples/tables/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import com.tsurugidb.sql.proto.SqlCommon;
 import com.tsurugidb.tsubakuro.exception.ServerException;
 import com.tsurugidb.tsubakuro.channel.common.connection.UsernamePasswordCredential;
 import com.tsurugidb.tsubakuro.common.Session;
@@ -27,24 +28,33 @@ public final class Main {
              .create(10, TimeUnit.SECONDS);
              SqlClient sqlClient = SqlClient.attach(session)) {
 
-            var searchPath = sqlClient.getSearchPath().get();
+            var searchPath = sqlClient.getSearchPath().await();
             System.out.println("---- schema names ----");
             for (var s : searchPath.getSchemaNames()) {
                 System.out.println("  " + s);
             }
-            var tableList = sqlClient.listTables().get();
+
+            var tableList = sqlClient.listTables().await();
+            System.out.println("---- simple names ----");
+            for (var t : tableList.getSimpleNames(searchPath)) {
+                System.out.println(" SimpleName = " + t);
+            }
+            System.out.println("---- table names ----");
             for (var t : tableList.getTableNames()) {
-                var metadata = sqlClient.getTableMetadata(t).get();
-                System.out.println("---- table name = " + t + " ----");
+                var metadata = sqlClient.getTableMetadata(t).await();
+                System.out.println(" table name = " + t);
                 var dn = metadata.getDatabaseName();
                 var sn = metadata.getSchemaName();
                 var tn = metadata.getTableName();
-                System.out.println("DatabaseName = " + (dn.isPresent() ? dn.get() : "DatabaseName not exist"));
-                System.out.println("SchemaName = " + (sn.isPresent() ? sn.get() : "SchemaName not exist"));
-                System.out.println("TableName = " + tn);
+                System.out.println("  DatabaseName = " + (dn.isPresent() ? dn.get() : "DatabaseName not exist"));
+                System.out.println("  SchemaName = " + (sn.isPresent() ? sn.get() : "SchemaName not exist"));
+                System.out.println("  TableName = " + tn);
+                System.out.println("  column");
                 for (var c : metadata.getColumns()) {
-                    System.out.println("column = " + c);
-                    System.out.println();
+                    System.out.println("   name = " + c.getName());
+                    if (c.getTypeInfoCase() == SqlCommon.Column.TypeInfoCase.ATOM_TYPE) {
+                        System.out.println("   type = " + c.getAtomType());
+                    }
                 }                    
             }
 
