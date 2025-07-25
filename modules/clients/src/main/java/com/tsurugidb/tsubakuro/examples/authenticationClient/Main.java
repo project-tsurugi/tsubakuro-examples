7124c7ab4b522bf233c18fb42d1065279ea3f1c1
t-horikawa <horikawa@nautilus-technologies.com>
2025-07-25 17:54:54 +0900

    sleep time for authenticationClient can be specified with command option

--- modules/clients/src/main/java/com/tsurugidb/tsubakuro/examples/authenticationClient/Main.java
+++ modules/clients/src/main/java/com/tsurugidb/tsubakuro/examples/authenticationClient/Main.java
@@ -31,7 +31,7 @@ public final class Main {
     private static long timeout = 500;  // milliseconds
 
     private static boolean useToken = false;
-    private static boolean waitForWhile = false;
+    private static int sleepTime = 0;
     private static boolean wrongPassword = false;
     private static String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJyZWZyZXNoIiwiYXVkIjoiYXV0aGVudGljYXRpb24tbWFuYWdlciIsInRzdXJ1Z2kvYXV0aC9uYW1lIjoidXNlciIsImlzcyI6ImF1dGhlbnRpY2F0aW9uLW1hbmFnZXIiLCJleHAiOjE3NTA4MzE4MTksImlhdCI6MTc1MDc0NTQxOSwianRpIjoiZGQ4MjU4OGMtMWFiNC00MTNjLThhNGYtZDkxYmYyOGU5OTRlIn0.ifRlaNF742JDkWOx1DlijeE-CazKCHXikxxR2BUv6V1YobquEvGo6Wei0cbTCo45NABKXDd8Zrf0S7u8ARuMla4aQZksXgq0a96YfYDRd-c7VYPR8JxBf7mBF3Jtu3NBP6bhzDU-b0kytp9kURAnlJ2GFK8Z-3EwiDaHjWTnA3Y9W6PLPLfaO3f1t2XgNyU3MK0zXz_6Qyy_Yo9fyAhiPCxQPtyXRRwJqw43A77n3HLDb0geI4-5f10MJ7mlZUDvTW6CXTtZyizyyREAqbcpV_OghKKJF2-tSWMud9aBIejxA3f5RSJhziX7vwXc72WQqiZYJWbzpNL2RQOhHhRbYg";
 
@@ -41,7 +41,7 @@ public final class Main {
 
         options.addOption(Option.builder("t").argName("use token").desc("use token.").build());
         options.addOption(Option.builder("w").argName("wrong password").desc("wrong password.").build());
-        options.addOption(Option.builder("z").argName("wait").desc("wait.").build());
+        options.addOption(Option.builder("s").argName("sleep").hasArg().desc("sleep a while.").build());
 
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = null;
@@ -57,9 +57,9 @@ public final class Main {
                 wrongPassword = true;
                 System.err.println("wrong password");
             }
-            if (cmd.hasOption("z")) {
-                waitForWhile = true;
-                System.err.println("sleep for a while");
+            if (cmd.hasOption("s")) {
+                sleepTime = Integer.parseInt(cmd.getOptionValue("s"));
+                System.err.println("sleep for " + sleepTime + " seconds");
             }
         } catch (ParseException e) {
             System.err.println("cmd parser failed." + e);
@@ -73,9 +73,9 @@ public final class Main {
              .create(timeout, TimeUnit.MILLISECONDS);
              SqlClient authenticationClient = SqlClient.attach(session); ) {
 
-            if (waitForWhile) {
+            if (sleepTime > 0) {
                 try {
-                    Thread.sleep(90000);
+                    Thread.sleep(sleepTime);
                 } catch(InterruptedException e){
                     e.printStackTrace();
                 }
