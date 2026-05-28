fef9ae60250c34672a048fcb7d05129fe922a134
t-horikawa <horikawa@nautilus-technologies.com>
2026-05-28 20:38:24 +0900

    enhance examples/blob to add the ability to inject faults

--- modules/clients/src/main/java/com/tsurugidb/tsubakuro/examples/blob/Main.java
+++ modules/clients/src/main/java/com/tsurugidb/tsubakuro/examples/blob/Main.java
@@ -5,9 +5,11 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Stream;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -15,6 +17,7 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import com.tsurugidb.sql.proto.SqlRequest;
 import com.tsurugidb.sql.proto.SqlCommon;
@@ -25,12 +28,16 @@ import com.tsurugidb.tsubakuro.common.BlobTransferType;
 import com.tsurugidb.tsubakuro.common.LargeObjectClient;
 import com.tsurugidb.tsubakuro.common.Session;
 import com.tsurugidb.tsubakuro.common.SessionBuilder;
+import com.tsurugidb.tsubakuro.sql.BlobReference;
+import com.tsurugidb.tsubakuro.sql.ClobReference;
 import com.tsurugidb.tsubakuro.sql.SqlClient;
 import com.tsurugidb.tsubakuro.sql.PreparedStatement;
 import com.tsurugidb.tsubakuro.sql.Placeholders;
 import com.tsurugidb.tsubakuro.sql.Parameters;
 import com.tsurugidb.tsubakuro.sql.Transaction;
 import com.tsurugidb.tsubakuro.sql.ResultSet;
+import com.tsurugidb.tsubakuro.sql.impl.BlobReferenceForSql;
+import com.tsurugidb.tsubakuro.sql.impl.ClobReferenceForSql;
 import com.tsurugidb.tsubakuro.util.FutureResponse;
 
 public final class Main {
@@ -45,11 +52,14 @@ public final class Main {
     private static long timeout = 500;  // milliseconds
 
     private static boolean query = false;
-    private static boolean createFile = true;
+    //    private static boolean createFile = true;
     private static boolean privileged = false;
     private static boolean createTable = false;
     private static boolean useLargeObjectClient = false;
     private static boolean useStream = false;
+    private static boolean doesNotUse = false;
+    private static boolean largeData = false;
+    private static char faultType = 'n';
 
     private final static String TABLE = "testTable";
     private final static String KEY = "1";
@@ -59,11 +69,14 @@ public final class Main {
         Options options = new Options();
 
         options.addOption(Option.builder("q").argName("query").desc("Query mode.").build());
-        options.addOption(Option.builder("n").argName("noCreateFile").desc("Does not create file.").build());
+        //        options.addOption(Option.builder("n").argName("noCreateFile").desc("Does not create file.").build());
         options.addOption(Option.builder("p").argName("privileged").desc("Use privileged blob transfer.").build());
         options.addOption(Option.builder("i").argName("initialize (createTable)").desc("Create table first.").build());
         options.addOption(Option.builder("c").argName("use LargeObjectClient").desc("Use LargeObjectClient.").build());
         options.addOption(Option.builder("s").argName("use LargeObjectClient stream").desc("Use LargeObjectClient stream.").build());
+        options.addOption(Option.builder("d").argName("does not use BLOB").desc("does not use BLOB.").build());
+        options.addOption(Option.builder("l").argName("large data").desc("use large test data.").build());
+        options.addOption(Option.builder("f").argName("fault injection").hasArg().desc("fault injection.").build());
 
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = null;
@@ -73,9 +86,6 @@ public final class Main {
 
             if (cmd.hasOption("q")) {
                 query = true;
-            }
-            if (cmd.hasOption("n")) {
-                createFile = false;
             }
             if (cmd.hasOption("p")) {
                 privileged = true;
@@ -89,6 +99,18 @@ public final class Main {
             if (cmd.hasOption("s")) {
                 useStream = true;
             }
+            if (cmd.hasOption("d")) {
+                doesNotUse = true;
+            }
+            if (cmd.hasOption("l")) {
+                largeData = true;
+            }
+            if (cmd.hasOption("f")) {
+                faultType = cmd.getOptionValue("f").charAt(0);
+            }
+            //            if (cmd.hasOption("n")) {
+            //                createFile = false;
+            //            }
         } catch (ParseException e) {
             System.err.println("cmd parser failed." + e);
             return;
@@ -116,17 +138,18 @@ public final class Main {
         try (
              Session session = SessionBuilder.connect(url)
              .withCredential(new UsernamePasswordCredential("user", "pass"))
-             .withBlobTransfer(privileged ? BlobTransferType.PRIVILEGED : BlobTransferType.RELAY)
+             .withBlobTransfer(doesNotUse ? BlobTransferType.DOES_NOT_USE : (privileged ? BlobTransferType.PRIVILEGED : BlobTransferType.RELAY))
              .create(timeout, TimeUnit.MILLISECONDS);
              SqlClient sqlClient = SqlClient.attach(session);
              var transaction = sqlClient.createTransaction().get(timeout, TimeUnit.MILLISECONDS)) {
 
             var blobTransferMedium = session.getBlobTransferMedium();
-            System.out.println(blobTransferMedium.getBlobTransferType());
-            System.out.println(blobTransferMedium.getParameters());
 
             PreparedStatement preparedStatement = null;
             if (query) {
+                System.out.println(blobTransferMedium.getBlobTransferType());
+                System.out.println(blobTransferMedium.getParameters());
+
                 preparedStatement = sqlClient.prepare("SELECT * FROM " + TABLE + " WHERE key = " + KEY).await();
 
                 var resultSet = transaction.executeQuery(preparedStatement).await();
@@ -152,28 +175,58 @@ public final class Main {
                                 System.out.println("column name = " + metadata.get(columnIndex).getName() + ": content = '" + resultSet.fetchCharacterValue() + "'");
                                 break;
                             case BLOB:
-                                var blobColumn = resultSet.fetchBlob();
+                                BlobReference blobColumn = resultSet.fetchBlob();
+                                switch (faultType) {
+                                case 'o':
+                                    blobColumn = new BlobReferenceForSql(SqlCommon.LargeObjectProvider.forNumber((int) blobColumn.getProvider()), blobColumn.getObjectId() + 1111, blobColumn.getReferenceTag());
+                                    break;
+                                case 't':
+                                    blobColumn = new BlobReferenceForSql(SqlCommon.LargeObjectProvider.forNumber((int) blobColumn.getProvider()), blobColumn.getObjectId(), blobColumn.getReferenceTag() + 1111);
+                                    break;
+                                }
                                 var is = transaction.openInputStream(blobColumn).await();
                                 System.out.print("column name = " + metadata.get(columnIndex).getName() + ": content = '");
+                                long blobSize = 0;
                                 while (true) {
                                     var i = is.read();
                                     if (i < 0) {
+                                        if (largeData) {
+                                            System.out.print("size = " + blobSize + " byte");
+                                        }
                                         break;
                                     }
-                                    char c = (char) i;
-                                    if (c != '\n') {
-                                        System.out.print(c);
+                                    if (!largeData) {
+                                        char c = (char) i;
+                                        if (c != '\n') {
+                                            System.out.print(c);
+                                        }
                                     }
+                                    blobSize++;
                                 }
                                 System.out.println("'");
                                 break;
                             case CLOB:
-                                var clobColumn = resultSet.fetchClob();
+                                ClobReference clobColumn = resultSet.fetchClob();
+                                switch (faultType) {
+                                case 'o':
+                                    clobColumn = new ClobReferenceForSql(SqlCommon.LargeObjectProvider.forNumber((int) clobColumn.getProvider()), clobColumn.getObjectId() + 1111, clobColumn.getReferenceTag());
+                                    break;
+                                case 't':
+                                    clobColumn = new ClobReferenceForSql(SqlCommon.LargeObjectProvider.forNumber((int) clobColumn.getProvider()), clobColumn.getObjectId(), clobColumn.getReferenceTag() + 1111);
+                                    break;
+                                }
                                 var lr = new LineNumberReader(transaction.openReader(clobColumn).await());
+                                long clobSize = 0;
                                 System.out.print("column name = " + metadata.get(columnIndex).getName() + ": content = '");
                                 String s;
                                 while ((s = lr.readLine()) != null) {
-                                    System.out.print(s);
+                                    if (!largeData) {
+                                        System.out.print(s);
+                                    }
+                                    clobSize += (s.length() + 1);
+                                }
+                                if (largeData) {
+                                    System.out.print("size = " + clobSize + " byte");
                                 }
                                 System.out.println("'");
                                 break;
@@ -188,14 +241,31 @@ public final class Main {
                     System.out.println("====");
                 }
                 resultSet.close();
+
             } else {
+
                 String[] fileNameArray = {"/tmp/testChannelBlob1Up.data", "/tmp/testChannelBlob2Up.data", "/tmp/testChannelClobUp.data"};
                 byte[] data = new byte[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                                            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
                 for (var n: fileNameArray) {
                     var path = Paths.get(n);
-                    if (Files.notExists(path) && createFile) {
-                        Files.write(Paths.get(n), data);
+                    
+                    if (Files.exists(path)) {
+                        Files.delete(path);
+                    }
+
+                    if (largeData) {
+                        if (Files.notExists(path)) {
+                            createHugeFile(path, 10);
+                        } else {
+                            throw new AssertionError();
+                        }
+                    } else {
+                        if (Files.notExists(path)) {
+                            Files.write(Paths.get(n), data);
+                        } else {
+                            throw new AssertionError();
+                        }
                     }
                 }
                 preparedStatement = sqlClient.prepare("INSERT INTO " + TABLE + " (key, blob_column_1, blob_column_2, clob_column) VALUES (" + KEY + ", :blob1, :blob2, :clob)",
@@ -203,32 +273,56 @@ public final class Main {
                                                       Placeholders.of("blob2", SqlCommon.Blob.class),
                                                       Placeholders.of("clob", SqlCommon.Clob.class)).await();
 
+                SqlRequest.Parameter p1;
+                SqlRequest.Parameter p2;
+                SqlRequest.Parameter p3;
+                    
                 if (useLargeObjectClient) {
                     System.err.println("use LargeObjectClient");
                     LargeObjectClient largeObjectClient = session.getLargeObjectClient();
+                    System.out.println(largeObjectClient);
 
                     if (useStream) {
                         System.err.println("use Stream for LargeObjectClient");
 
-                        var is0 = new FileInputStream(fileNameArray[0]);
-                        var is1 = new FileInputStream(fileNameArray[1]);
-                        var r2 = new InputStreamReader(new FileInputStream(fileNameArray[2]));
-                        transaction.executeStatement(preparedStatement,
-                                                     Parameters.blobOf("blob1", largeObjectClient.upload(is0).await()),
-                                                     Parameters.blobOf("blob2", largeObjectClient.upload(is1).await()),
-                                                     Parameters.clobOf("clob", largeObjectClient.upload(r2).await())).await();
+                        p1 = Parameters.blobOf("blob1", largeObjectClient.upload(new FileInputStream(fileNameArray[0])).await());
+                        p2 = Parameters.blobOf("blob2", largeObjectClient.upload(new FileInputStream(fileNameArray[1])).await());
+                        p3 = Parameters.clobOf("clob", largeObjectClient.upload(new InputStreamReader(new FileInputStream(fileNameArray[2]))).await());
                     } else {
-                        transaction.executeStatement(preparedStatement,
-                                                     Parameters.blobOf("blob1", largeObjectClient.upload(Paths.get(fileNameArray[0])).await()),
-                                                     Parameters.blobOf("blob2", largeObjectClient.upload(Paths.get(fileNameArray[1])).await()),
-                                                     Parameters.clobOf("clob", largeObjectClient.upload(Paths.get(fileNameArray[2])).await())).await();
+                        p1 = Parameters.blobOf("blob1", largeObjectClient.upload(Paths.get(fileNameArray[0])).await());
+                        p2 = Parameters.blobOf("blob2", largeObjectClient.upload(Paths.get(fileNameArray[1])).await());
+                        p3 = Parameters.clobOf("clob", largeObjectClient.upload(Paths.get(fileNameArray[2])).await());
+                    }
+                    switch (faultType) {
+                    case 'o':
+                        var ref1 = p1.getLargeObjectInfoBlob().getBlobRelayReference();
+                        p1 = SqlRequest.Parameter.newBuilder()
+                            .setName(p1.getName())
+                            .setLargeObjectInfoBlob(SqlRequest.ClientOnlyLargeObjectInfo.newBuilder()
+                                                    .setBlobRelayReference(SqlRequest.BlobRelayReference.newBuilder()
+                                                                           .setStorageId(ref1.getStorageId())
+                                                                           .setObjectId(ref1.getObjectId() + 1111)
+                                                                           .setTag(ref1.getTag())))
+                            .build();
+                        break;
+                    case 't':
+                        var ref2 = p2.getLargeObjectInfoBlob().getBlobRelayReference();
+                        p2 = SqlRequest.Parameter.newBuilder()
+                            .setName(p2.getName())
+                            .setLargeObjectInfoBlob(SqlRequest.ClientOnlyLargeObjectInfo.newBuilder()
+                                                    .setBlobRelayReference(SqlRequest.BlobRelayReference.newBuilder()
+                                                                           .setStorageId(ref2.getStorageId())
+                                                                           .setObjectId(ref2.getObjectId())
+                                                                           .setTag(ref2.getTag() + 1111)))
+                            .build();
+                        break;
                     }
                 } else {
-                    transaction.executeStatement(preparedStatement,
-                                                 Parameters.blobOf("blob1", Paths.get(fileNameArray[0])),
-                                                 Parameters.blobOf("blob2", Paths.get(fileNameArray[1])),
-                                                 Parameters.clobOf("clob", Paths.get(fileNameArray[2]))).await();
+                    p1 = Parameters.blobOf("blob1", Paths.get(fileNameArray[0]));
+                    p2 = Parameters.blobOf("blob2", Paths.get(fileNameArray[1]));
+                    p3 = Parameters.clobOf("clob", Paths.get(fileNameArray[2]));
                 }
+                transaction.executeStatement(preparedStatement, p1, p2, p3).await();
 
                 System.out.println("insertion [b|c]lob to key " + KEY + " completed");
             }
@@ -237,5 +331,12 @@ public final class Main {
             e.printStackTrace();
             System.err.println(e);
         }
+    }
+
+    static void createHugeFile(final Path path, int size) throws IOException {
+        Iterable<String> lines = () -> Stream
+            .generate(() -> RandomStringUtils.randomAlphanumeric(size))
+            .limit(1000000).iterator();
+        Files.write(path, lines);
     }
 }
